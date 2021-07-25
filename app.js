928510e29751acda4a52f9f32a7d6f41d68db7cb
shadow-oss <55443830+shadow-oss@users.noreply.github.com>
2021-07-25 14:21:55 +0500

    commit

--- app.js
+++ app.js
@@ -2,29 +2,35 @@ function getResult(){
     var cp=parseInt(document.querySelector("#txt1").value);
     var qty=parseInt(document.querySelector("#txt2").value);
     var sp=parseInt(document.querySelector("#txt3").value);
+    var result=document.querySelector("#result");
+    var back=document.querySelector("#container");
     var pl,percent;
     if(cp>0 && sp>0 && qty>0){
         if(cp>sp){
             pl=(cp-sp)*qty;
             percent=(((cp-sp)*100)/cp).toFixed(2);
-            console.log("Loss of:",pl,percent);
+            result.innerHTML="You lost "+percent+"%.Your total loss is ₹"+pl;
+            back.style.backgroundImage="url('cashfull.jpg')";
             if(percent>50){
                 console.log("sad theme");
+                back.style.backgroundImage="url('fire-15.gif')";
+                
+                
             }
         }
         else if(sp>cp){
             pl=(sp-cp)*qty;
             percent=(((sp-cp)*100)/cp).toFixed(4);
-            console.log("profit of:",pl,percent);
+            result.innerHTML="You gain "+percent+"%.Your total Profit is ₹"+pl;
+            back.style.backgroundImage="url('cashfull.jpg')";
             if(percent>50){
                 console.log("Happy theme");
-                document.querySelector("#container").style.backgroundImage="url('fire-17.gif')";
-                document.querySelector("#container").style.backgroundColor="black";
+                back.style.backgroundImage="url('celeb.gif')";
             }
         }
     }    
     else{
-        console.log("error no zero");
+        console.log("no zero");
     }
 
  }
